fe716197e133a24553441277c5db1c49f430b372
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-06-28 13:36:41 +0900

    Added database setting

---
+++ srcs/requirements/database/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "Initializing MariaDB..."
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql >/dev/null
+fi
+
+cat <<EOF >/tmp/init.sql
+CREATE DATABASE IF NOT EXISTS \`${WP_DATABASE_NAME}\`;
+CREATE USER IF NOT EXISTS '${WP_DATABASE_USER}'@'%' IDENTIFIED BY '${WP_DATABASE_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${WP_DATABASE_NAME}\`.* TO '${WP_DATABASE_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+echo "Starting MariaDB..."
+exec mysqld_safe --user=mysql --init-file=/tmp/init.sql
